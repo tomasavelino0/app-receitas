c81a66d4454a1cf884d07c65546f91076be1f14b
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-09-23 18:54:59 -0300

    Finalizando req 8

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import { bool, string } from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title = 'Header', isRenderProfile = false, isRenderSearch = false }) {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile');
+  };
+
   return (
     <header>
       <h1 data-testid="page-title">{`Título da Página ${title}`}</h1>
       {isRenderProfile
-        ? <img data-testid="profile-top-btn" src={ profileIcon } alt="profile icon" />
+        ? (
+          <button onClick={ handleClick } type="button">
+            <img
+              data-testid="profile-top-btn"
+              src={ profileIcon }
+              alt="profile icon"
+            />
+          </button>
+        )
         : null}
       {isRenderSearch
         ? <img data-testid="search-top-btn" src={ searchIcon } alt="search icon" />
