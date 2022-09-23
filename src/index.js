bba69cac47ee13e92c1fac4e0e5109d06c04d038
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-09-23 18:19:39 -0300

    Recriando branch dev

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
