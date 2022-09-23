38a3f8b1535fdb076deacdae9fb6955d84a80482
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-09-23 17:11:30 -0300

    Finalizando requisito 07

---
+++ src/pages/Drinks Recipes Progress.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DrinksRecipesProgress() {
+  return (
+    <div>
+      Drinks Recipes Progress
+    </div>
+  );
+}
+
+export default DrinksRecipesProgress;
---
+++ src/pages/Drinks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Drinks() {
+  return (
+    <div>
+      <Header isRenderSearch isRenderProfile title="Drinks" />
+    </div>
+  );
+}
+
+export default Drinks;
