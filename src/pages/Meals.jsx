00f25c29f0c574059b91ebba2af75eb2e8ca26a2
jhomgarcia <abranges1@gmail.com>
2022-09-26 15:57:39 -0300

    requisito 16, 17, 18 - OK

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Meals() {
   return (
     <div>
       <Header isRenderSearch isRenderProfile title="Meals" />
+      <Footer />
     </div>
   );
 }
